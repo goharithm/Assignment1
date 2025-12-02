c708144a9945d3b20c8b4fe2e49ad184060a909a
Goharithm <goharithm@gmail.com>
2025-12-02 18:14:30 +0200

    first test

--- assignment.js
+++ assignment.js
@@ -1,3 +1,22 @@
-console.log("hello world")
-console.log("test that!!")
-console.log("i love nvim")
+//Convert the string "123" to a number and add 7. (0.5 Grade)
+num = "123"
+console.log(Number(num)+7);
+
+//Check if the given variable is falsy and return "Invalid" if it is. (0.5 Grade)
+let value = 0 
+if (!value) {
+  console.log("invalid");
+}
+
+
+//Use for loop to print all numbers between 1 and 10, skipping even numbers using continue (0.5 Grade)
+for (let index=1 ; index <= 10 ; index++) {
+  if (index % 2 ===0) {
+    continue ;
+    
+  }
+  console.log(index)
+}
+
+
+
